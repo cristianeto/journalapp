50ea195700f239af7d2a71ea44adae187ed0402e
Cristian Guaman <cristian.guaman@icloud.com>
2021-05-19 12:10:03 -0500

    Seccion 18. JournalApp 222. Estilos del LoginScreen

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const LoginScreen = () => {
   return (
     <>
-      <h3>Login</h3>
+      <h3 className='auth__title'>Login</h3>
       <form>
-        <input type='text' name='email' placeholder='email' />
-        <input type='password' name='password' placeholder='password' />
-        <button type='submit'>Login</button>
-        <hr />
-        Google
+        <input
+          autoComplete='off'
+          className='auth__input'
+          type='text'
+          name='email'
+          placeholder='Email'
+        />
+        <input
+          className='auth__input'
+          type='password'
+          name='password'
+          placeholder='Password'
+        />
+        <button className='btn btn-primary btn-block' type='submit'>
+          Login
+        </button>
+
+        <div className='auth__social-networks'>
+          <p>Login with social networks</p>
+          <div className='google-btn'>
+            <div className='google-icon-wrapper'>
+              <img
+                className='google-icon'
+                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
+                alt='google button'
+              />
+            </div>
+            <p className='btn-text'>
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <Link to='/auth/register'> Create new account </Link>
       </form>
     </>
   );
