9a978f590bf893894a694624b630f5186d883ee2
Cristian Guaman <cristian.guaman@icloud.com>
2021-05-31 16:11:06 -0500

    Seccion 18. JournalApp 226. Componente cuando no hay nada seleccionado

---
+++ src/components/journal/NothingSelected.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NothingSelected = () => {
+  return (
+    <div className='nothing__main-content'>
+      <p>Select a entry</p>
+      or
+      <p>Create a new entry</p>
+      <i className='far fa-star fa-4x mt-5'></i>
+    </div>
+  );
+};
+
+export default NothingSelected;
