e7c5cb4fb5dec6b2db9b504474aca9ef3abcdc09
Cristian Guaman <cristian.guaman@icloud.com>
2021-05-31 14:43:22 -0500

    Seccion 18. JournalApp 223. Estilos en el RegisterScreen

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const RegisterScreen = () => {
   return (
-    <div>
-      <h1>Register Screen</h1>
-    </div>
+    <>
+      <h3 className='auth__title'>Register</h3>
+      <form>
+        <input
+          autoComplete='off'
+          className='auth__input'
+          type='text'
+          name='email'
+          placeholder='Email'
+        />
+        <input
+          className='auth__input'
+          type='text'
+          name='name'
+          placeholder='Name'
+        />
+        <input
+          className='auth__input'
+          type='password'
+          name='password'
+          placeholder='Password'
+        />
+        <input
+          className='auth__input'
+          type='password'
+          name='confirm_password'
+          placeholder='Confirm password'
+        />
+        <button className='btn btn-primary btn-block mb-5' type='submit'>
+          Register
+        </button>
+
+        <Link to='/auth/register' className='link'>
+          {' '}
+          Already register?
+        </Link>
+      </form>
+    </>
   );
 };
 
