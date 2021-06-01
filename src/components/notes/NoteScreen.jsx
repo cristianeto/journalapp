6d919dbc372c1e6342bab45ef31bcd1274cd6cf9
Cristian Guaman <cristian.guaman@icloud.com>
2021-06-01 12:29:13 -0500

    Seccion 18. JournalApp 228. Estructura y diseño del HomeScreen

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -5,6 +5,25 @@ const NoteScreen = () => {
   return (
     <div className='notes__main-content'>
       <NotesAppbar />
+      <div className='notes__content'>
+        <input
+          autoComplete='off'
+          type='text'
+          name='title'
+          className='notes__title-input'
+          placeholder='Some awesome title'
+        />
+        <textarea
+          placeholder='What happened today'
+          className='notes__textarea'
+        ></textarea>
+        <div className='notes__image'>
+          <img
+            src='https://images.immediate.co.uk/production/volatile/sites/22/2019/06/Englsih-oak-in-a-field-d2ece6a.png?quality=45&resize=620,413'
+            alt='tree'
+          />
+        </div>
+      </div>
     </div>
   );
 };
