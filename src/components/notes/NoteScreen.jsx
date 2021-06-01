5caea2f54f7462bad6f6b689737cb0b80ffb9cf4
Cristian Guaman <cristian.guaman@icloud.com>
2021-06-01 10:36:43 -0500

    Seccion 18. JournalApp 227. NoteAppbar

---
+++ src/components/notes/NoteScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NotesAppbar from './NotesAppbar';
+
+const NoteScreen = () => {
+  return (
+    <div className='notes__main-content'>
+      <NotesAppbar />
+    </div>
+  );
+};
+
+export default NoteScreen;
---
+++ src/components/notes/NotesAppbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NotesAppbar = () => {
+  return (
+    <div className='notes__appbar'>
+      <span> 01 junio 2021</span>
+      <div>
+        <button className='btn'>Picture</button>
+        <button className='btn'>Save</button>
+      </div>
+    </div>
+  );
+};
+
+export default NotesAppbar;
