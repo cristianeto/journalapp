7e50f39eaa66a59f79525460665d163e5f06b40a
Cristian Guaman <cristian.guaman@icloud.com>
2021-06-01 16:40:27 -0500

    Seccion 20. JournalApp 238. Primer dispatch de una acción a nuestro Store

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { login } from '../../actions/auth';
+import { useForm } from '../../hooks/useForm';
 const LoginScreen = () => {
+  const dispatch = useDispatch();
+
+  const [formValues, handleInputChange] = useForm({
+    email: 'cris@gmail.com',
+    password: '123456',
+  });
+  const { email, password } = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login(12345, 'Gerardo'));
+  };
+
   return (
     <>
       <h3 className='auth__title'>Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           autoComplete='off'
           className='auth__input'
           type='text'
           name='email'
           placeholder='Email'
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           className='auth__input'
           type='password'
           name='password'
           placeholder='Password'
+          value={password}
+          onChange={handleInputChange}
         />
         <button className='btn btn-primary btn-block' type='submit'>
           Login
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -11,7 +11,7 @@ export const authReducer = (state = {}, action) => {
     case types.login:
       return {
         uid: action.payload.uid,
-        name: action.payload.name,
+        name: action.payload.displayName,
       };
     case types.logout:
       return {};
