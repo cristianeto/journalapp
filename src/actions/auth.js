beb6569de05b8302de656e07703d22252b2392ef
Cristian Guaman <cristian.guaman@icloud.com>
2021-06-07 16:09:13 -0500

    Seccion 20. JournalApp 247. Realizar el login de usuario con correo y contraseña

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,9 +3,15 @@ import { firebase, googleAuthProvider } from '../firebase/config';
 
 export const startLoginEmailPassword = (email, password) => {
   return (dispatch) => {
-    setTimeout(() => {
-      dispatch(login(123, 'Geova'));
-    }, 3500);
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        dispatch(login(user.uid, user.displayName));
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 };
 
