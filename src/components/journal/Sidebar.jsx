340c604288717b389f18b34d4adf4b58a6efb7b4
Cristian Guaman <cristian.guaman@icloud.com>
2021-06-15 12:15:57 -0500

    Sección 21. 260. Crear una nueva nota

--- src/components/journal/Sidebar.jsx
+++ src/components/journal/Sidebar.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { startLogout } from '../../actions/auth';
 
 import JournalEntries from './JournalEntries';
+import { startNewNote } from '../../actions/notes';
 
 const Sidebar = () => {
   const dispatch = useDispatch();
@@ -10,6 +11,10 @@ const Sidebar = () => {
 
   const handleLogout = () => {
     dispatch(startLogout());
+  };
+
+  const handleAddEntry = () => {
+    dispatch(startNewNote());
   };
   return (
     <aside className='journal__sidebar'>
@@ -22,7 +27,7 @@ const Sidebar = () => {
           Logout
         </button>
       </div>
-      <div className='journal__new-entry'>
+      <div className='journal__new-entry' onClick={handleAddEntry}>
         <i className='far fa-calendar-plus fa-5x'></i>
         <p className='mt-5'>New entry</p>
       </div>
