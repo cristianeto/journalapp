1335d6773cef09621c45796e953559be908733dd
Cristian Guaman <cristian.guaman@icloud.com>
2021-06-16 13:34:12 -0500

    Sección 21. 264. Mostrar las notas en el menu lateral

--- src/components/journal/JournalEntry.jsx
+++ src/components/journal/JournalEntry.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import moment from 'moment';
 
-const JournalEntry = () => {
+const JournalEntry = ({ id, date, title, body, url }) => {
+  const noteDate = moment(date);
+  console.log(noteDate);
   return (
     <div className='journal__entry pointer'>
-      <div
-        className='journal__entry-picture'
-        style={{
-          backgroundSize: 'cover',
-          backgroundImage:
-            'url(https://jsequeiros.com/sites/default/files/imagen-cachorro-comprimir.jpg)',
-        }}
-      ></div>
+      {url && (
+        <div
+          className='journal__entry-picture'
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url(${url})`,
+          }}
+        ></div>
+      )}
       <div className='journal__entry-body'>
-        <p className='journal__entry-title'>Lorem ipsum dolor</p>
-        <p className='journal__entry-content'>
-          Quibusdam quis cumque iure repellendus
-        </p>
+        <p className='journal__entry-title'>{title}</p>
+        <p className='journal__entry-content'>{body}</p>
       </div>
       <div className='journal__entry-date-box'>
-        <span>Monday</span>
-        <h4>28</h4>
+        <span>{noteDate.format('dddd')}</span>
+        <h4>{noteDate.format('Do')}</h4>
       </div>
     </div>
   );
